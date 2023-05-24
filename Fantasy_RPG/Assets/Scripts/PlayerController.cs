6d067df0f3f29de0efadbc834a9cd1955d98b3a5
ChangHee <ckdgml586@gmail.com>
2023-05-24 17:31:43 +0900

    플레이어 이동, 카메라 회전 적용

---
+++ Fantasy_RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform characterBody;
+    [SerializeField]
+    private float applySpeed;
+    [SerializeField]
+    private Transform cameraArm;
+
+    [SerializeField]
+    private float LookSensitivity;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        LookAround();
+        Move();
+    }
+
+    void LookAround()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * LookSensitivity;
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+
+        //카메라 각도 제한
+        float x = camAngle.x - mouseDelta.y;
+        if (x < 180f)
+        {
+            x = Mathf.Clamp(x, -1f, 70f);
+        }
+        else
+        {
+            x = Mathf.Clamp(x, 350f, 361f);
+        }
+
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+    }
+
+    void Move()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+        Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+        Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+        characterBody.forward = moveDir;
+        transform.position += moveDir * Time.deltaTime * applySpeed;
+    }
+
+}
